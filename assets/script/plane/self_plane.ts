b41cf5b7a6979155ff8ab394a02bb3d307a9196b
chuyan1 <379482455@qq.com>
2023-06-20 23:51:29 +0800

    添加飞机对象

---
+++ assets/script/plane/self_plane.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node} from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('self_plane')
+export class self_plane extends Component {
+
+    start() { 
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
