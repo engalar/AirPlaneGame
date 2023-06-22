3eb56c6376c1aa1898212d2d71f956d13799c396
chuyan1 <379482455@qq.com>
2023-06-22 19:07:11 +0800

    添加飞机射击功能

--- assets/script/ui/ui_main.ts
+++ assets/script/ui/ui_main.ts
@@ -1,4 +1,5 @@
 import { _decorator, Component, Node, Input, EventTouch } from 'cc';
+import { game_manager } from '../framework/game_manager';
 const { ccclass, property } = _decorator;
 
 @ccclass('ui_main')
@@ -9,14 +10,23 @@ export class ui_main extends Component {
     @property(Node)
     public player_plane: Node = null;
 
+    @property(game_manager)
+    public game_manager_ptr: game_manager = null;
+
     start() {
+        this.node.on(Input.EventType.TOUCH_START, this.touch_start, this);
         this.node.on(Input.EventType.TOUCH_MOVE, this.touch_move, this);
+        this.node.on(Input.EventType.TOUCH_END, this.touch_end, this);
     }
 
     update(deltaTime: number) {
         
     }
-
+    
+    touch_start(event: EventTouch){
+        this.game_manager_ptr.is_shooting(true);
+    }
+    
     touch_move(event: EventTouch){
         const delta = event.getDelta();
         let pos = this.player_plane.position;
@@ -24,5 +34,9 @@ export class ui_main extends Component {
             pos.x + 0.01 * this.plane_speed * delta.x, 
             pos.y, 
             pos.z - 0.01 *this.plane_speed * delta.y);
+    }
+
+    touch_end(event: EventTouch){
+        this.game_manager_ptr.is_shooting(false);
     }
 }
