168f64be776dd242805fc7d8c3e368addf404c7a
chuyan1 <379482455@qq.com>
2023-06-25 22:31:59 +0800

    更新项目

--- assets/script/ui/ui_main.ts
+++ assets/script/ui/ui_main.ts
@@ -5,28 +5,28 @@ const { ccclass, property } = _decorator;
 @ccclass('ui_main')
 export class ui_main extends Component {
     @property
-    public plane_speed = 4;
+    public plane_speed = 2;
 
     @property(Node)
     public player_plane: Node = null;
 
     @property(game_manager)
     public game_manager_ptr: game_manager = null;
-
+    
     start() {
         this.node.on(Input.EventType.TOUCH_START, this.touch_start, this);
         this.node.on(Input.EventType.TOUCH_MOVE, this.touch_move, this);
-        this.node.on(Input.EventType.TOUCH_END, this.touch_end, this);
+        this.node.on(Input.EventType.TOUCH_END, this.touch_end, this);           
     }
-
+    
     update(deltaTime: number) {
         
     }
-    
+     
     touch_start(event: EventTouch){
         this.game_manager_ptr.is_shooting(true);
     }
-    
+   
     touch_move(event: EventTouch){
         const delta = event.getDelta();
         let pos = this.player_plane.position;
